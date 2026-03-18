0d1fc34c05571295371715faed8848b7904647e9
lenakononchuk <lena@rockingtech.co.uk>
2026-03-18 18:32:44 +0200

    fixed animation

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import ScrollParallax from 'vue3-parallax/src/components/ScrollParallax.vue';
-import './assets/styles/app.less'  
+import './assets/styles/app.less'
 import LazyBg from './directives/lazyBackgrounds.js'
 import scrollAnimate from './utils/useScrollAnimation.js'
 
@@ -10,7 +10,7 @@ import scrollAnimate from './utils/useScrollAnimation.js'
 
 const app = createApp(App)
 // scrollToGeo globally
-app.directive('scroll-animate', scrollAnimate)
+// app.directive('scroll-animate', scrollAnimate)
 app.directive('lazy-bg', LazyBg)
 app.component('scroll-parallax', ScrollParallax);
 app.mount('#app')
